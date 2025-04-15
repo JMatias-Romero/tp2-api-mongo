31accefaf898478bf60a00e247da27b0decbce74
Matias Romero <jmatias.romero@gmail.com>
2025-04-15 08:30:23 -0300

    se actualiza el paquete mongo

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,26 +2,6 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.model');
 
-// Middleware para chequear si el usuario esta autenticado
-const getUser = async (req, res, next) => {
-    let user;
-    const { id } = req.params;
-
-    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-        return res.status(400).json({ message: 'El ID del usuario es invalido' });
-    }
-    try {
-        user = await User.findById(id)
-        if (!user) {
-            return res.status(404).json({ message: 'Uuario no encontrado' });
-        }
-    } catch (error) {
-        return res.status(500).json({ message: error.message });
-    }
-    req.user = user;
-    next();
-}
-
 //get de todos los usuarios
 router.get('/', async (req, res) => {
     try {
